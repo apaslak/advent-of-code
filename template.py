81edc5150a61af784e410076a1314973cbc1f605
Amy Paslak <1447904+apaslak@users.noreply.github.com>
2023-12-26 13:33:37 -0600

    new template and pylintrc

---
+++ template.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+def method(param):
+    pass
+
+
+def test_examples():
+    assert method(12) == 2
+    assert method(14) == 2
+    assert method(1969) == 654
+    assert method(100756) == 33583
+
+
+def puzzle1():
+    answer = 0
+    with open(r'input.txt', 'r', encoding="utf-8") as input_file:
+        data = input_file.readlines()
+
+        for d in data:
+            answer += method(int(d.strip()))
+
+    print(f'{answer}')
+
+
+if __name__ == '__main__':
+    test_examples()
+    puzzle1()
